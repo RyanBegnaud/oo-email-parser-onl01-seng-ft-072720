313dca13b76d7719db564c3dbd20559257ee1438
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-07 17:53:41 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,6 +7,8 @@ class EmailAddressParser
   attr_accessor :email_addresses 
   def initialize(email_addresses) 
     @email_addresses = email_addresses
+    @email_addresses
+  end
 
 def parse
   binding.pry
