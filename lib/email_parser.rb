660183d40f00f19a26c81d5bb20fbae3c1087613
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-07 17:48:11 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,7 +5,7 @@
 class EmailAddressParser
   attr_accessor :email_addresses 
 
-def parse(email_addresses)
+def parse
   binding.pry
   if email_addresses.include?(",")
     email_addresses.split(",")
