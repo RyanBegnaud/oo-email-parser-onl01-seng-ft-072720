92de38b3855a4b9ddce05c916677602e30464088
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-07 18:15:34 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ def parse
   binding.pry
   if @email_addresses.include?(",")
    parsed = @email_addresses.split(",")
-   
+   .split /\s|'|,/
    parsed = parsed.reject { |element| element.empty? }
    parsed = parsed.collect(&:strip)
    parsed.uniq!
